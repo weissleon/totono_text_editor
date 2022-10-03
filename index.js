771bc679ca1db9a8a7f0b3626d6918f204785377
weissleon <weissleon1943@gmail.com>
2022-10-03 11:45:48 +0900

    Update internal logic

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const PATH_DIR_SRC = "./src";
 const PATH_DIR_TRANS = "./trans";
 const PATH_DIR_OUT = "./out";
 
+async function getTranslationDataCsv() {
+  const fs = require("fs/promises");
+  const path = require("path");
+  const csvText = await fs.readFile(
+    path.join(PATH_DIR_TRANS, "translation_data.csv"),
+    { encoding: "utf-8" }
+  );
+
+  const Papa = require("papaparse");
+  const result = Papa.parse(csvText, { delimiter: ",", skipEmptyLines: true });
+  const data = result.data
+    .map((datum) => [datum[0], datum[3], datum[5]])
+    .filter((datum) => datum[1] !== "");
+  return data;
+}
+
 async function getTranslationData() {
   const ExcelJS = require("exceljs");
   const path = require("path");
@@ -36,48 +52,199 @@ async function getTranslationData() {
   return translationData;
 }
 
+async function translate(srcText, translationData) {
+  const refStartTag = "/*<JP>";
+  const refEndTag = "*/";
+  const swapStartTag = '$"';
+  const swapEndTag = '";';
+
+  const lines = srcText.split(/\r\n/);
+
+  let isRef = false;
+  let isSwapArea = false;
+  let isTransPlanted = false;
+  let src = "";
+  let translation = "";
+  let builder = [];
+
+  let finalBuilder = [];
+
+  const prompts = require("prompts");
+
+  for (const line of lines) {
+    // When the src is one-liner
+    if (line.includes(refStartTag) && line.includes(refEndTag)) {
+      const startIdx = line.indexOf(refStartTag) + refStartTag.length;
+      const endIdx = line.indexOf(refEndTag);
+
+      src = line.substring(startIdx, endIdx);
+      console.log("one-liner");
+      console.log("src:\n" + src);
+      finalBuilder.push(line);
+      continue;
+    }
+
+    if (line.includes(swapStartTag) && line.includes(swapEndTag)) {
+      const startIdx = line.indexOf(swapStartTag) + swapStartTag.length;
+      const endIdx = line.indexOf(swapStartTag);
+
+      console.log(`The src is ${src}`);
+      const datum = translationData.filter((datum) => {
+        // console.log(datum[1]);
+        return datum[1] === src;
+      });
+
+      let translation = null;
+      if (datum.length === 0) {
+        const response = await prompts([
+          {
+            type: "text",
+            name: "translation",
+            message: `Unmatched segment found:\n${src}\n\nHow would you like to replace?`,
+            initial: src,
+          },
+        ]);
+        translation = response.translation;
+      } else {
+        translation = datum[0][2];
+      }
+
+      const swapData = line.slice(0, startIdx) + translation + swapEndTag;
+      console.log("one-liner");
+      console.log("trans:\n" + translation);
+      finalBuilder.push(swapData);
+      continue;
+    }
+
+    if (line.includes(swapStartTag)) {
+      isSwapArea = true;
+      translation = "";
+      finalBuilder.push(line);
+      continue;
+    }
+
+    if (line.includes(swapEndTag) && isSwapArea) {
+      isSwapArea = false;
+      isTransPlanted = false;
+      finalBuilder.push(line);
+      continue;
+    }
+
+    if (line.includes(refStartTag)) {
+      isRef = true;
+      src = "";
+      finalBuilder.push(line);
+      continue;
+    }
+
+    if (line.includes(refEndTag) && isRef) {
+      isRef = false;
+      src = builder.join("\r\n");
+      builder = [];
+      console.log("src:\n" + src);
+      finalBuilder.push(line);
+      continue;
+    }
+
+    if (isSwapArea) {
+      if (line.includes("//") || line.match(/<.+>$/)) {
+        finalBuilder.push(line);
+        continue;
+      }
+      if (!isTransPlanted) {
+        const datum = translationData.filter(
+          (datum) => datum[1].replace("\n", "\r\n") === src
+        );
+        let translation = null;
+
+        console.log(`datum: ${datum}`);
+
+        if (datum.length === 0) {
+          const response = await prompts([
+            {
+              type: "text",
+              name: "translation",
+              message: `Unmatched segment found:\n${src}\n\nHow would you like to replace?`,
+              initial: src,
+            },
+          ]);
+          translation = response.translation;
+        } else {
+          translation = datum[0][2];
+        }
+        finalBuilder.push(translation);
+        isTransPlanted = true;
+        isSwapArea = true;
+      }
+      continue;
+    }
+
+    if (isRef) {
+      if (!line.includes("//") && !line.match(/<.+>$/)) builder.push(line);
+      finalBuilder.push(line);
+      continue;
+    }
+
+    finalBuilder.push(line);
+  }
+
+  const finalText = finalBuilder.join("\r\n");
+
+  return finalText;
+}
+
 async function run() {
   const filePath = process.argv[2];
   const path = require("path");
-  const translationData = await getTranslationData();
+  const translationData = await getTranslationDataCsv();
 
   const fs = require("fs/promises");
   const srcText = await fs.readFile(filePath, { encoding: "utf-8" });
 
-  console.log(srcText);
+  const finalText = await translate(srcText, translationData);
 
-  const startTag = "/*<JP>";
-  const endTag = "*/";
-
-  let transText = "";
-  let cursor = 0;
-
-  if (srcText.indexOf(startTag) === -1) {
-    transText = srcText;
-    await fs.writeFile(path.join(PATH_DIR_OUT, path.basename(filePath)));
-    return;
+  try {
+    await fs.access(PATH_DIR_OUT);
+  } catch (error) {
+    await fs.mkdir(PATH_DIR_OUT);
   }
 
-  while (true) {
-    transText += srcText.substring(
-      cursor,
-      srcText.indexOf(startTag) + startTag.length
-    );
-    cursor = srcText.indexOf(startTag) + startTag.length;
+  await fs.writeFile(
+    path.join(PATH_DIR_OUT, path.basename(filePath)),
+    finalText
+  );
 
-    const jp = srcText
-      .substring(cursor, srcText.indexOf(endTag))
-      .replace(/\n/, "");
-    console.log("Japanese:", jp);
+  // console.log(srcText);
 
-    transText += srcText.substring(
-      cursor,
-      srcText.indexOf(endTag) + endTag.length
-    );
-    cursor = srcText.indexOf(endTag) + endTag.length;
+  // let transText = "";
+  // let cursor = 0;
 
-    transText += srcText.substring(cursor, srcText.indexOf(`$"`));
-  }
+  // if (srcText.indexOf(refStartTag) === -1) {
+  //   transText = srcText;
+  //   await fs.writeFile(path.join(PATH_DIR_OUT, path.basename(filePath)));
+  //   return;
+  // }
+
+  // while (true) {
+  //   transText += srcText.substring(
+  //     cursor,
+  //     srcText.indexOf(startTag) + startTag.length
+  //   );
+  //   cursor = srcText.indexOf(startTag) + startTag.length;
+
+  //   const jp = srcText
+  //     .substring(cursor, srcText.indexOf(endTag))
+  //     .replace(/\n/, "");
+  //   console.log("Japanese:", jp);
+
+  //   transText += srcText.substring(
+  //     cursor,
+  //     srcText.indexOf(endTag) + endTag.length
+  //   );
+  //   cursor = srcText.indexOf(endTag) + endTag.length;
+
+  //   transText += srcText.substring(cursor, srcText.indexOf(`$"`));
+  // }
 }
 
 run();
